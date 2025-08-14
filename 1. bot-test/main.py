e8ee983f6732b78ca12ec38652c65ff5ac086d62
AdrianGerman <adrian.german1019@gmail.com>
2025-08-14 09:33:24 -0600

    init config for my first test bot

---
+++ 1. bot-test/main.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+import requests
+import secret  # env variable from secrets file
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"Estamos dentro! {bot.user}")
+
+
+bot.run(secret.TOKEN)
