4936bb5bf9677ed92b49119cc826d0f23db54ea1
AdrianGerman <adrian.german1019@gmail.com>
2025-08-15 08:52:37 -0600

    test command

--- 1. bot-test/main.py
+++ 1. bot-test/main.py
@@ -9,6 +9,11 @@ intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 
+@bot.command()
+async def test(ctx, *arg):
+    await ctx.send(" ".join(arg))
+
+
 @bot.event
 async def on_ready():
     print(f"Estamos dentro! {bot.user}")
